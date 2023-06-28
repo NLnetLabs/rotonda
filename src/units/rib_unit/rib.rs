37d474ab95a414b1895d1d1b8f4d7bdc80ed1a74
ximon18 <3304436+ximon18@users.noreply.github.com>
2023-06-28 10:28:05 +0000

    Tidy up the RibValue changes.

--- src/units/rib_unit/rib.rs
+++ src/units/rib_unit/rib.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::hash_set,
     hash::{BuildHasher, Hasher},
+    net::IpAddr,
     ops::Deref,
     sync::Arc,
 };
@@ -13,6 +14,7 @@ use roto::types::{
     typevalue::TypeValue,
 };
 use rotonda_store::{prelude::MergeUpdate, MultiThreadedStore};
+use routecore::asn::Asn;
 use serde::Serialize;
 use smallvec::SmallVec;
 
@@ -90,28 +92,22 @@ impl PhysicalRib {
 /// 1. The outer Arc is used to prevent costly deep copying of the HashSet when `Store::match_prefix()` clones the
 /// metadata value of matching prefixes into its `prefix_meta`, `less_specifics` and `more_specifics` fields.
 ///
-/// 2. The inner Arc is used to prevent costly deep copying of the HashSet items. The type `PreHashedTypeValue`
-/// contains an inner `RawRouteWithDeltas` field which is not cheap to clone because it holds two `Vec` instances: one
-/// in the `AttributeDeltaList` type and one in the ``RouteStatusDeltaList` type. To use RibValue as the metadata value
-/// type of a MultiThreadedStore it must implement the MergeUpdate trait and thus must implement the
-/// `clone_merge_update()` function, which as the name implies is required to clone itself. Items in the HashSet which
-/// need not be changed by the `MultiThreadedStore::insert()` operation (that invoked `clone_merge_update()`) need not be
-/// deeply copied, but the HashSet itself should not be modified via interior mutability in such a way that the prior
-/// metadata value is also modified by the `clone_merge_update()` call, so we must not clone the outer Arc. We don't
-/// however need to deep copy every item stored in the HashSet, only those which we want to modify, so we use an Arc
-/// around the HashSet items to avoid paying a penalty for deep copying the aforementioned Vecs.
+/// 2. The inner Arc is used to prevent costly deep copying of the HashSet items. To use RibValue as the metadata value
+/// type of a MultiThreadedStore it must implement the MergeUpdate trait and thus must implement `clone_merge_update()`
+/// but the `PreHashedTypeValue` inner `TypeValue` is not cheap to clone. However, items in the HashSet which need not
+/// be changed by the `MultiThreadedStore::insert()` operation (that invoked `clone_merge_update()`) need not be
+/// deeply copied, we only need to "modify" zero or more items in the HashSet that are affected by the update, where
+/// "affected" is type dependent. Note that the HashSet itself should not be modified via interior mutability in such a
+/// way that the prior metadata value is also modified by the `clone_merge_update()` call. Rather than deep copy every
+/// item stored in the HashSet just to possibly modify some of them, we can insteasd use an Arc around the HashSet items
+/// so that cloning the HashSet doesn't unnecessarily deep clone the items. For items that do have to be modified we
+/// will have to clone the value inside the Arc around the HashSet item, but for the rest we can just clone the Arc.
 ///
 /// 3. A HashedSet is used instead of a HashSet because HashedSet is a handy way to construct a HashSet with a no-op
 /// hash function. We use this because the key of the items that we store will in future be determined by roto script
 /// and not hard-coded in Rust types. We therefore precompute a hash code value and store it with the actual metadata
 /// value and the Hash trait impl passes the precomputed hash code to the HashedSet hasher which uses it effectively
 /// as-is, to avoid pointlessly calculating yet another hash code as would happen with the default Hasher.
-///
-/// Note: the actual inner metadata value type stored is expected to change in future from RawRouteWithDeltas to
-/// a record type defined by roto script. This type could be a super set of what we have now, e.g. it could record
-/// details about where we learned a route from (e.g. BGP peer IP address and ASN and/or BMP monitored router ID), or
-/// it could be a subset of what we have now, e.g. rather than an entire BGP UPDATE it might just be a few key fields
-/// in order to avoid the memory cost of storing a bunch of route details that are never going to be used.
 #[derive(Debug, Clone, Default, PartialEq)]
 pub struct RibValue {
     per_prefix_items: Arc<HashedSet<Arc<PreHashedTypeValue>>>,
@@ -145,68 +141,32 @@ impl MergeUpdate for RibValue {
     where
         Self: std::marker::Sized,
     {
-        let new_per_prefix_item: &PreHashedTypeValue =
-            update_meta.per_prefix_items.iter().next().unwrap();
-        let new_per_prefix_potential_route: &TypeValue = new_per_prefix_item;
-        let new_per_prefix_items = match new_per_prefix_potential_route {
+        // There should only ever be one so unwrap().
+        let in_item: &TypeValue = update_meta.per_prefix_items.iter().next().unwrap();
+
+        // Clone ourselves, withdrawing matching routes if the given item is a withdrawn route
+        let out_items: HashedSet<Arc<PreHashedTypeValue>> = match in_item {
             TypeValue::Builtin(BuiltinTypeValue::Route(new_route))
                 if new_route.status() == RouteStatus::Withdrawn =>
             {
-                // For routes, if it is an announce we replace or insert an existing route with the same hash key,
-                // just like for any other type of RIB item. For a withdrawal however, we don't replace it but instead
-                // update it with RouteStatus::Withdrawn. If we were to replace the entire route we would lose
-                // information about it because withdrawal BGP UPDATE messages only state the prefix to withdraw a route
-                // to, they don't say which path that route follows.
+                let peer_id = PeerId::new(new_route.peer_ip(), new_route.peer_asn());
 
-                // We're withdrawing a route. We can't locate the route to withdraw by its usual hash key as that is
-                // partly based on the AS path which is not part of a withdrawal, for a withdrawal we only know the
-                // originating peer and the prefix to which routes are being withdrawn.
-                //
-                // So: for each route to this prefix:
                 self.per_prefix_items
                     .iter()
-                    .map(|v| {
-                        // See if it was originated by the same peer as the withdrawal we received:
-                        let stored_phtv_ref = Arc::deref(v);
-                        let to_update_ty_ref = stored_phtv_ref.deref();
-                        match to_update_ty_ref {
-                            TypeValue::Builtin(BuiltinTypeValue::Route(to_update_route))
-                                if to_update_route.peer_ip() == new_route.peer_ip()
-                                    && to_update_route.peer_asn() == new_route.peer_asn() =>
-                            {
-                                // It was, so clone the value behind the Arc and mark it as withdrawn (which requires
-                                // mutable access to the route so can't be done via the Arc).
-                                let delta_id = (RotondaId(0), 0); // TODO
-                                let mut cloned_route = to_update_route.clone();
-                                cloned_route.update_status(delta_id, RouteStatus::Withdrawn);
-                                let cloned_tv: TypeValue = cloned_route.into();
-                                let cloned_phtv = PreHashedTypeValue::new(
-                                    cloned_tv,
-                                    stored_phtv_ref.precomputed_hash,
-                                ); // TODO: surely cloning the precomputed hash after modifying the value is a bad idea?
-                                Arc::new(cloned_phtv)
-                            }
-
-                            _ => v.clone(), // clone Arc
-                        }
-                    })
-                    .collect::<HashedSet<Arc<PreHashedTypeValue>>>()
-                    .into() // into Arc
+                    .map(|route| route.clone_and_withdraw(peer_id))
+                    .collect::<_>()
             }
 
             _ => {
-                // For all other cases, just use the Hash impl to replace an existing item or to insert a new item.
+                // For all other cases, just use the Eq/Hash impls to replace matching or insert new.
                 self.per_prefix_items
                     .union(&update_meta.per_prefix_items)
                     .cloned()
-                    .collect::<HashedSet<Arc<PreHashedTypeValue>>>()
-                    .into() // into Arc
+                    .collect::<_>()
             }
         };
 
-        Ok(Self {
-            per_prefix_items: new_per_prefix_items,
-        })
+        Ok(out_items.into())
     }
 }
 
@@ -234,6 +194,14 @@ impl From<PreHashedTypeValue> for RibValue {
     }
 }
 
+impl From<HashedSet<Arc<PreHashedTypeValue>>> for RibValue {
+    fn from(value: HashedSet<Arc<PreHashedTypeValue>>) -> Self {
+        Self {
+            per_prefix_items: Arc::new(value),
+        }
+    }
+}
+
 // -------- PreHashedTypeValue ----------------------------------------------------------------------------------------
 
 #[derive(Debug, Clone, Serialize)]
@@ -254,6 +222,23 @@ impl PreHashedTypeValue {
             precomputed_hash,
         }
     }
+
+    pub fn is_route_from_peer(&self, peer_id: PeerId) -> bool {
+        self.peer_id() == Some(peer_id)
+    }
+
+    pub fn clone_and_withdraw(
+        self: &Arc<PreHashedTypeValue>,
+        peer_id: PeerId,
+    ) -> Arc<PreHashedTypeValue> {
+        if self.peer_id() == Some(peer_id) {
+            let mut cloned = Arc::deref(self).clone();
+            cloned.withdraw();
+            Arc::new(cloned)
+        } else {
+            Arc::clone(self)
+        }
+    }
 }
 
 impl std::ops::Deref for PreHashedTypeValue {
@@ -261,6 +246,12 @@ impl std::ops::Deref for PreHashedTypeValue {
 
     fn deref(&self) -> &Self::Target {
         &self.value
+    }
+}
+
+impl std::ops::DerefMut for PreHashedTypeValue {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
     }
 }
 
@@ -281,6 +272,44 @@ impl PartialEq for PreHashedTypeValue {
 
 impl Eq for PreHashedTypeValue {}
 
+// --- Route related helpers ------------------------------------------------------------------------------------------
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct PeerId {
+    pub ip: Option<IpAddr>,
+    pub asn: Option<Asn>,
+}
+
+impl PeerId {
+    fn new(ip: Option<IpAddr>, asn: Option<Asn>) -> Self {
+        Self { ip, asn }
+    }
+}
+
+trait RouteExtra {
+    fn withdraw(&mut self);
+
+    fn peer_id(&self) -> Option<PeerId>;
+}
+
+impl RouteExtra for TypeValue {
+    fn withdraw(&mut self) {
+        if let TypeValue::Builtin(BuiltinTypeValue::Route(route)) = self {
+            let delta_id = (RotondaId(0), 0); // TODO
+            route.update_status(delta_id, RouteStatus::Withdrawn);
+        }
+    }
+
+    fn peer_id(&self) -> Option<PeerId> {
+        match self {
+            TypeValue::Builtin(BuiltinTypeValue::Route(route)) => {
+                Some(PeerId::new(route.peer_ip(), route.peer_asn()))
+            }
+            _ => None,
+        }
+    }
+}
+
 // --- Tests ----------------------------------------------------------------------------------------------------------
 
 #[cfg(test)]
@@ -292,11 +321,11 @@ mod tests {
         typevalue::TypeValue,
     };
     use rotonda_store::prelude::MergeUpdate;
-    use routecore::{addr::Prefix, bgp::message::SessionConfig};
+    use routecore::{addr::Prefix, asn::Asn, bgp::message::SessionConfig};
 
     use crate::bgp::encode::{mk_bgp_update, Announcements, Prefixes};
 
-    use super::{PreHashedTypeValue, RibValue};
+    use super::{PeerId, PreHashedTypeValue, RibValue};
 
     #[test]
     fn empty_by_default() {
@@ -342,39 +371,52 @@ mod tests {
 
     #[test]
     fn merging_in_a_withdrawal_updates_matching_entries() {
+        // Given route announcements and withdrawals from a couple of peers to a single prefix
+        let prefix = Prefix::new("127.0.0.1".parse().unwrap(), 32).unwrap();
+        
+        let peer_one = PeerId::new(
+            Some(IpAddr::from_str("192.168.0.1").unwrap()),
+            Some(Asn::from_u32(123)),
+        );
+        let peer_two = PeerId::new(
+            Some(IpAddr::from_str("192.168.0.2").unwrap()),
+            Some(Asn::from_u32(456)),
+        );
+        
+        let peer_one_announcement_one = mk_route_announcement(prefix, "123,456,789", peer_one);
+        let peer_one_announcement_two = mk_route_announcement(prefix, "123,789", peer_one);
+        let peer_two_announcement_one = mk_route_announcement(prefix, "456,789", peer_two);
+        let peer_one_withdrawal = mk_route_withdrawal(prefix, peer_one);
+        
+        let peer_one_announcement_one = PreHashedTypeValue::new(peer_one_announcement_one.into(), 1);
+        let peer_one_announcement_two = PreHashedTypeValue::new(peer_one_announcement_two.into(), 2);
+        let peer_two_announcement_one = PreHashedTypeValue::new(peer_two_announcement_one.into(), 3);
+        let peer_one_withdrawal = PreHashedTypeValue::new(peer_one_withdrawal.into(), 4);
+
+        // When merged into a RibValue        
         let rib_value = RibValue::default();
 
-        let prefix = Prefix::new("127.0.0.1".parse().unwrap(), 32).unwrap();
-        let peer_one = IpAddr::from_str("192.168.0.1").unwrap();
-        let peer_two = IpAddr::from_str("192.168.0.2").unwrap();
-        let announcement_one = mk_route_announcement(prefix, "123,456").with_peer_ip(peer_one);
-        let announcement_two = mk_route_announcement(prefix, "789,456").with_peer_ip(peer_one);
-        let announcement_three = mk_route_announcement(prefix, "123,456").with_peer_ip(peer_two);
-        let withdrawal = mk_route_withdrawal(prefix).with_peer_ip(peer_one);
-
-        let announcement_one = PreHashedTypeValue::new(announcement_one.into(), 1);
-        let announcement_two = PreHashedTypeValue::new(announcement_two.into(), 2);
-        let announcement_three = PreHashedTypeValue::new(announcement_three.into(), 3);
-        let withdrawal = PreHashedTypeValue::new(withdrawal.into(), 1);
-
+        // Unique announcements accumulate in the RibValue
         let rib_value = rib_value
-            .clone_merge_update(&announcement_one.into())
+            .clone_merge_update(&peer_one_announcement_one.into())
             .unwrap();
         assert_eq!(rib_value.len(), 1);
 
         let rib_value = rib_value
-            .clone_merge_update(&announcement_two.into())
+            .clone_merge_update(&peer_one_announcement_two.into())
             .unwrap();
         assert_eq!(rib_value.len(), 2);
 
         let rib_value = rib_value
-            .clone_merge_update(&announcement_three.into())
+            .clone_merge_update(&peer_two_announcement_one.into())
             .unwrap();
         assert_eq!(rib_value.len(), 3);
 
-        let rib_value = rib_value.clone_merge_update(&withdrawal.into()).unwrap();
+        // And a withdrawal of one of those announced routes leaves the RibValue size unchanged
+        let rib_value = rib_value.clone_merge_update(&peer_one_withdrawal.into()).unwrap();
         assert_eq!(rib_value.len(), 3);
 
+        // And routes from the first peer which were withdrawn are marked as such
         let mut iter = rib_value.iter();
         let first = iter.next();
         assert!(first.is_some());
@@ -384,7 +426,8 @@ mod tests {
             TypeValue::Builtin(BuiltinTypeValue::Route(_))
         ));
         if let TypeValue::Builtin(BuiltinTypeValue::Route(route)) = first_ty {
-            assert_eq!(route.peer_ip(), Some(peer_one));
+            assert_eq!(route.peer_ip(), Some(peer_one.ip.unwrap()));
+            assert_eq!(route.peer_asn(), Some(peer_one.asn.unwrap()));
             assert_eq!(route.status(), RouteStatus::Withdrawn);
         }
 
@@ -396,10 +439,12 @@ mod tests {
             TypeValue::Builtin(BuiltinTypeValue::Route(_))
         ));
         if let TypeValue::Builtin(BuiltinTypeValue::Route(route)) = next_ty {
-            assert_eq!(route.peer_ip(), Some(peer_one));
+            assert_eq!(route.peer_ip(), Some(peer_one.ip.unwrap()));
+            assert_eq!(route.peer_asn(), Some(peer_one.asn.unwrap()));
             assert_eq!(route.status(), RouteStatus::Withdrawn);
         }
 
+        // But the route from the second peer remains untouched
         let next = iter.next();
         assert!(next.is_some());
         let next_ty: &TypeValue = next.unwrap().deref();
@@ -408,12 +453,13 @@ mod tests {
             TypeValue::Builtin(BuiltinTypeValue::Route(_))
         ));
         if let TypeValue::Builtin(BuiltinTypeValue::Route(route)) = next_ty {
-            assert_eq!(route.peer_ip(), Some(peer_two));
+            assert_eq!(route.peer_ip(), Some(peer_two.ip.unwrap()));
+            assert_eq!(route.peer_asn(), Some(peer_two.asn.unwrap()));
             assert_eq!(route.status(), RouteStatus::InConvergence);
         }
     }
 
-    fn mk_route_announcement(prefix: Prefix, as_path: &str) -> RawRouteWithDeltas {
+    fn mk_route_announcement(prefix: Prefix, as_path: &str, peer_id: PeerId) -> RawRouteWithDeltas {
         let delta_id = (RotondaId(0), 0);
         let announcements = Announcements::from_str(&format!(
             "e [{as_path}] 10.0.0.1 BLACKHOLE,123:44 {}",
@@ -424,26 +470,46 @@ mod tests {
 
         // When it is processed by this unit
         let bgp_update_msg = UpdateMessage::new(bgp_update_bytes, SessionConfig::modern());
-        RawRouteWithDeltas::new_with_message(
+        let mut route = RawRouteWithDeltas::new_with_message(
             delta_id,
             prefix.into(),
             bgp_update_msg,
             RouteStatus::InConvergence,
-        )
+        );
+
+        if let Some(ip) = peer_id.ip {
+            route = route.with_peer_ip(ip);
+        }
+
+        if let Some(asn) = peer_id.asn {
+            route = route.with_peer_asn(asn);
+        }
+
+        route
     }
 
-    fn mk_route_withdrawal(prefix: Prefix) -> RawRouteWithDeltas {
+    fn mk_route_withdrawal(prefix: Prefix, peer_id: PeerId) -> RawRouteWithDeltas {
         let delta_id = (RotondaId(0), 0);
         let bgp_update_bytes =
             mk_bgp_update(&Prefixes::new(vec![prefix]), &Announcements::None, &[]);
 
         // When it is processed by this unit
         let bgp_update_msg = UpdateMessage::new(bgp_update_bytes, SessionConfig::modern());
-        RawRouteWithDeltas::new_with_message(
+        let mut route = RawRouteWithDeltas::new_with_message(
             delta_id,
             prefix.into(),
             bgp_update_msg,
             RouteStatus::Withdrawn,
-        )
+        );
+
+        if let Some(ip) = peer_id.ip {
+            route = route.with_peer_ip(ip);
+        }
+
+        if let Some(asn) = peer_id.asn {
+            route = route.with_peer_asn(asn);
+        }
+
+        route
     }
 }

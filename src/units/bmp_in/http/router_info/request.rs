65577700ea36e18136b2c6dfaa5e652e077ffb1d
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2023-10-12 09:55:06 +0200

    Fix broken links in bmp-in HTML interface. (#33)
    And name the RIB instances whose APIs are linked to.

--- src/units/bmp_in/http/router_info/request.rs
+++ src/units/bmp_in/http/router_info/request.rs
@@ -62,7 +62,6 @@ impl ProcessRequest for RouterInfoApi {
         let req_path = request.uri().decoded_path();
         if request.method() == Method::GET && req_path.starts_with(self.http_api_path.deref()) {
             let (base_path, router) = req_path.split_at(self.http_api_path.len());
-            let router = router.trim_start_matches('/');
             if let Some(state_machine) = self.state_machine.upgrade() {
                 let lock = state_machine.lock().await;
                 let sm = lock.as_ref().unwrap();
@@ -107,7 +106,7 @@ impl ProcessRequest for RouterInfoApi {
                 {
                     return Some(Self::build_response(
                         self.http_resources.clone(),
-                        format!("{}/{}", base_path, router),
+                        format!("{}{}", base_path, router),
                         self.source_id.clone(),
                         router_id,
                         sys_name,

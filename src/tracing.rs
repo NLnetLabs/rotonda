54a15aef9bdb8f78c49c37f18026deb3bca8dfa2
ximon18 <3304436+ximon18@users.noreply.github.com>
2023-10-26 18:14:58 +0200

    Descirbe the tracing architecture a bit.

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,5 +1,35 @@
-//----------- Tracer ---------------------------------------------------------
-
+/// Support for tracing messages through the Rotonda pipeline.
+/// 
+/// Tracing is implemented as a limited pool of trace ID numbers for each of
+/// which a collection of arbitrary strings may be stored, each timestamped
+/// and associated with a component in the Rotonda pipeline. Collectively
+/// these attributes define a [`TraceMsg`] and a collection of them is called
+/// a [`Trace`] which traces the progress of pipeline messages relating to a
+/// particular trace ID through the pipeline. The complete set of traces is
+/// owned by a [`Tracer`] of which there is a single "global" instance,
+/// accessed via the [`Component`] instance passed to each unit when created.
+/// 
+/// Trace ID numbers are 8-bit unsigned values meaning we can store at most
+/// 256 traces at any given moment, and only need to pass an 8-bit value along
+/// the pipeline along with the messages being traced.
+/// 
+/// To add the most tracing support with the least code changes tracing is
+/// associated either with a Gate, as most components have a gate or receive
+/// messages from a Gate, or with the component that owns the Gate.
+/// 
+/// A limitation of this is that at present it is not possible to store
+/// messgaes that relate solely to a target, as they have no Gate except the
+/// Gate they receive messages from.
+/// 
+/// By distinguishing between the Gate and the component that owns it we can
+/// enable visualizing of messages as relating to the component or to the
+/// "sections of pipe" down which messages flow out from the component to its
+/// downstream recipients.
+/// 
+/// To avoid having to pass a [`Tracer`] and a [`Gate`] ID down to a deeper
+/// component so that it can record traces, the two can be bound together in
+/// a [`BoundTracer`] and only that need be passed down deeper into the
+/// application code.
 use std::sync::{
     atomic::{AtomicU8, Ordering::SeqCst},
     Arc, Mutex,
@@ -8,6 +38,9 @@ use std::sync::{
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
+//----------- MsgRelation ----------------------------------------------------
+
+/// To which component does a trace message relate?
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum MsgRelation {
     /// The message relates to the Gate.
@@ -19,6 +52,8 @@ pub enum MsgRelation {
     /// All.
     ALL,
 }
+
+//----------- TraceMsg -------------------------------------------------------
 
 #[derive(Clone, Debug)]
 pub struct TraceMsg {
@@ -43,6 +78,8 @@ impl TraceMsg {
         }
     }
 }
+
+//----------- Trace ----------------------------------------------------------
 
 #[derive(Clone, Debug)]
 pub struct Trace {
@@ -92,6 +129,8 @@ impl Trace {
         &self.msgs
     }
 }
+
+//----------- Tracer ---------------------------------------------------------
 
 pub struct Tracer {
     traces: Arc<Mutex<[Trace; 256]>>,
@@ -155,6 +194,8 @@ impl Default for Tracer {
         Self::new()
     }
 }
+
+//----------- BoundTracer ----------------------------------------------------
 
 #[derive(Clone, Debug)]
 pub struct BoundTracer {

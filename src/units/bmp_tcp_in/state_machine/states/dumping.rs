7cd3dea9719a30bcf34ce230ae5c7abdc996ca7d
Jasper den Hertog <jasper@plainspace.com>
2023-11-02 14:23:12 +0100

    counting

--- src/units/bmp_tcp_in/state_machine/states/dumping.rs
+++ src/units/bmp_tcp_in/state_machine/states/dumping.rs
@@ -934,12 +934,14 @@ mod tests {
                         if !distinct_bgp_updates_seen
                             .contains(&route.raw_message)
                         {
+                            eprintln!("withdrawals {:?}", route.raw_message.raw_message().0.withdrawals().unwrap().map(|n| n.unwrap()).collect::<Vec<_>>().len());
                             num_withdrawals_seen += route
                                 .raw_message
                                 .raw_message()
                                 .0
                                 .withdrawals()
-                                .iter()
+                                .unwrap()
+                                .filter_map(|n| if n.is_ok() { Some(()) } else { None })
                                 .count();
                             distinct_bgp_updates_seen
                                 .insert(route.raw_message.clone());

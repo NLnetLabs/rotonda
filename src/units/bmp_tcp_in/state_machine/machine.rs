eed9e3370f67a1b086a4b8e17bb01cf961bb3b98
Jasper den Hertog <jasper@plainspace.com>
2023-11-02 12:57:25 +0100

    Fix per peer header session config

--- src/units/bmp_tcp_in/state_machine/machine.rs
+++ src/units/bmp_tcp_in/state_machine/machine.rs
@@ -1,6 +1,6 @@
 use atomic_enum::atomic_enum;
 use bytes::Bytes;
-use chrono::{DateTime, Utc};
+use log::warn;
 use roto::types::builtin::RouteStatus;
 
 /// RFC 7854 BMP processing.
@@ -456,7 +456,16 @@ where
         msg: PeerUpNotification<Bytes>,
     ) -> ProcessingResult {
         let pph = msg.per_peer_header();
-        let config = msg.session_config();
+        let (config, inconsistent) = msg.pph_session_config();
+
+        if let Some((pph_4, bgp_4)) = inconsistent {
+            warn!(
+                "Four-Octxwet-ASN capability in encapsulated BGP OPEN message \
+                is '{:?}', but conflicts with the Per Peer Header. Ignoring \
+                BGP OPEN. Setting Four-Octet-ASN to '{:?}' for this BMP session.",
+                bgp_4, pph_4
+            );
+        }
 
         // Will this peer send End-of-RIB?
         let eor_capable = msg

c0ce5467a9848588bc66484dfeda107b7eb7339d
ximon18 <3304436+ximon18@users.noreply.github.com>
2023-11-16 10:10:10 +0100

    Extend the config test to verify client ID requirements enforcement, and a connection refused test that tests the connection established/lost metrics showing that the new testability works.

--- src/targets/mqtt/tests.rs
+++ src/targets/mqtt/tests.rs
@@ -1,6 +1,9 @@
-use std::{net::IpAddr, str::FromStr, sync::Arc};
+use std::{net::IpAddr, str::FromStr, sync::Arc, time::Duration};
 
+use arc_swap::ArcSwap;
+use async_trait::async_trait;
 use bytes::Bytes;
+use mqtt::{ClientError, ConnectionError, Event, MqttOptions, Packet, QoS};
 use roto::types::{
     builtin::{
         BgpUpdateMessage, BuiltinTypeValue, RawRouteWithDeltas, RotondaId,
@@ -18,35 +21,67 @@ use routecore::{
     bmp::message::{Message as BmpMsg, PeerType},
 };
 use serde_json::json;
+use tokio::{sync::mpsc, time::Instant};
 
 use crate::{
     bgp::encode::{
         mk_bgp_update, mk_initiation_msg, mk_route_monitoring_msg,
         Announcements, MyPeerType, PerPeerHeader, Prefixes,
     },
-    manager::Component,
+    comms::Terminated,
+    manager::{Component, TargetCommand},
     payload::{Payload, SourceId},
-    tests::util::assert_json_eq,
+    targets::mqtt::config::ClientId,
+    tests::util::{
+        assert_json_eq,
+        internal::{enable_logging, get_testable_metrics_snapshot},
+    },
 };
 
-use super::{config::Config, target::*};
+use super::{
+    config::Config,
+    connection::{Client, Connection, ConnectionFactory, EventLoop},
+    status_reporter::MqttStatusReporter,
+    target::*,
+};
 
 #[test]
-fn server_host_config_setting_must_be_provided() {
-    let empty = r#""#;
-    let empty_destination = r#"destination = """#;
-    let destination_with_host_only = r#"destination = "some_host_name""#;
-    let destination_with_host_and_invalid_port =
-        r#"destination = "some_host_name:invalid_port""#;
-    let destination_with_host_and_port =
-        r#"destination = "some_host_name:12345""#;
+fn destination_and_client_id_config_settings_must_be_provided() {
+    let empty = r###""###;
+    let empty_destination = r###"
+        client_id = "some-client-id"
+        destination = ""
+    "###;
+    let empty_client_id = r###"
+        client_id = ""
+        destination = "some_host_name"
+    "###;
+    let leading_whitespace_client_id = r###"
+        client_id = " "
+        destination = "some_host_name"
+    "###;
+    let destination_with_host_only = r###"
+        client_id = "some-client-id"
+        destination = "some_host_name"
+    "###;
+    let destination_with_host_and_invalid_port = r###"
+        client_id = "some-client-id"
+        destination = "some_host_name:invalid_port"
+    "###;
+    let destination_with_host_and_port = r###"
+        client_id = "some-client-id"
+        destination = "some_host_name:12345"
+    "###;
 
     assert!(mk_config_from_toml(empty).is_err());
+    assert!(mk_config_from_toml(empty_client_id).is_err());
+    assert!(mk_config_from_toml(leading_whitespace_client_id).is_err());
     assert!(mk_config_from_toml(empty_destination).is_err());
-    assert!(mk_config_from_toml(destination_with_host_only).is_ok());
     assert!(
         mk_config_from_toml(destination_with_host_and_invalid_port).is_err()
     );
+
+    assert!(mk_config_from_toml(destination_with_host_only).is_ok());
     assert!(mk_config_from_toml(destination_with_host_and_port).is_ok());
 }
 
@@ -75,7 +110,133 @@ fn generate_correct_json_for_publishing_from_output_stream_roto_type_value() {
     assert_json_eq(actual_json, expected_json);
 }
 
+#[tokio::test]
+async fn connection_refused() {
+    enable_logging("trace");
+    let mut config = Config::default();
+    config.client_id = ClientId("conn-refused".to_string());
+    let config = Arc::new(ArcSwap::from_pointee(config));
+    let (runner, status_reporter) = MqttRunner::mock(config);
+    let runner = Arc::new(runner);
+
+    // If either tx or cmd_tx are dropped the spawned runner will exit, so
+    // we don't do let (_, ..) here as that would drop them immediately.
+    let (_tx, rx) = mpsc::unbounded_channel();
+    let (cmd_tx, cmd_rx) = mpsc::channel(100);
+
+    let spawned_runner = runner.clone();
+
+    let join_handle = tokio::spawn(async move {
+        spawned_runner
+            .do_run::<MockConnectionFactory>(None, cmd_rx, rx)
+            .await
+    });
+
+    const MAX_WAIT: Duration = Duration::from_secs(10);
+    let start_time = Instant::now();
+
+    while Instant::now().duration_since(start_time) < MAX_WAIT {
+        let metrics =
+            get_testable_metrics_snapshot(&status_reporter.metrics());
+        if metrics.with_name::<usize>("mqtt_target_connection_lost_count") > 0
+        {
+            break;
+        } else {
+            tokio::time::sleep(Duration::from_millis(100)).await;
+        }
+    }
+
+    cmd_tx.send(TargetCommand::Terminate).await.unwrap();
+    assert_eq!(join_handle.await.unwrap(), Err(Terminated));
+
+    let metrics = get_testable_metrics_snapshot(&status_reporter.metrics());
+    assert!(
+        metrics.with_name::<usize>("mqtt_target_connection_lost_count") > 0
+    );
+    assert_eq!(
+        metrics.with_name::<u8>("mqtt_target_connection_established"),
+        0
+    );
+}
+
 // --- Test helpers -----------------------------------------------------------------------------------------------
+
+#[derive(Clone, Debug)]
+struct MockClient;
+
+#[async_trait]
+impl Client for MockClient {
+    async fn publish<S, V>(
+        &self,
+        topic: S,
+        qos: QoS,
+        retain: bool,
+        payload: V,
+    ) -> Result<(), ClientError>
+    where
+        S: Into<String> + Send,
+        V: Into<Vec<u8>> + Send,
+    {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+struct MockEventLoop {
+    options: MqttOptions,
+}
+
+impl MockEventLoop {
+    fn new(options: MqttOptions) -> Self {
+        Self { options }
+    }
+}
+
+#[async_trait]
+impl EventLoop for MockEventLoop {
+    async fn poll(&mut self) -> Result<Event, ConnectionError> {
+        match self.options.client_id().as_str() {
+            "conn-refused" => Err(ConnectionError::ConnectionRefused(
+                mqtt::ConnectReturnCode::ServiceUnavailable,
+            )),
+
+            _ => Ok(Event::Incoming(Packet::PingReq)),
+        }
+    }
+
+    fn mqtt_options(&self) -> &MqttOptions {
+        &self.options
+    }
+}
+
+struct MockConnectionFactory;
+
+impl ConnectionFactory for MockConnectionFactory {
+    type EventLoopType = MockEventLoop;
+
+    type ClientType = MockClient;
+
+    fn connect(
+        config: &Config,
+        status_reporter: Arc<MqttStatusReporter>,
+    ) -> (MockClient, Connection<MockEventLoop>) {
+        let client = MockClient;
+
+        let options = MqttOptions::new(
+            config.client_id.clone(),
+            config.destination.host.clone(),
+            config.destination.port,
+        );
+
+        let connection = Connection::new(
+            MockEventLoop::new(options),
+            Duration::from_secs(1),
+            status_reporter,
+        );
+
+        (client, connection)
+    }
+}
 
 fn mk_mqtt_runner() -> MqttRunner {
     let config = Config {

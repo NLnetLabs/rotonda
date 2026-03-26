e7df5cc72afc97f8e8e411a3fb63dde49cacda10
Luuk Hendriks <luuk@nlnetlabs.nl>
2026-03-26 08:37:51 +0100

    v4 announcements using raw attributes working

--- src/units/bgp_tcp_in/http_ng.rs
+++ src/units/bgp_tcp_in/http_ng.rs
@@ -5,8 +5,17 @@ use axum::{
     response::IntoResponse,
 };
 use base64::prelude::*;
+use bytes::Bytes;
 use inetnum::{addr::Prefix, asn::Asn};
 use log::{debug, warn};
+use routecore::bgp::{
+    message::{
+        update_builder::UpdateBuilder, PduParseInfo, SessionConfig,
+        UpdateMessage,
+    },
+    nlri::afisafi::{Ipv4UnicastNlri, Ipv6UnicastNlri},
+    path_attributes::{OwnedPathAttributes, PaMap},
+};
 use serde::Deserialize;
 //use serde_json::Value;
 
@@ -28,9 +37,14 @@ pub fn register_routes(router: &mut Api) {
 
 #[derive(Debug, Deserialize)]
 struct Announce {
-    prefix: Prefix,
+    prefix: Vec<Prefix>,
     // base64 encoded path attributes
     raw_attributes: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Withdraw {
+    prefix: Vec<Prefix>,
 }
 
 async fn send_pdu_for_ingress_id(
@@ -83,22 +97,74 @@ fn send_pdu(
 
     debug!("in send_pdu, global_live_sessions: {:#?}", sessions.keys());
 
-    if let Some(_tx) = sessions.get(&(remote_addr, remote_asn)) {
-        debug!(
-            "found a session for {remote_asn}@{remote_addr} to send a PDU to"
-        );
-        let decoded = BASE64_STANDARD
-            .decode(body.raw_attributes.as_bytes())
-            .unwrap();
-
-        Ok(format!(
-            "found a session, TODO send announcement for {} containing {:?}",
-            body.prefix, decoded
-        ))
-    } else {
+    let Some((_tx_cmds, tx_pdus)) = sessions.get(&(remote_addr, remote_asn))
+    else {
         warn!("no live BGP session for {remote_asn}@{remote_addr}");
-        Err(ApiError::InternalServerError(
+        return Err(ApiError::InternalServerError(
             "no live BGP session for {remote_asn}@{remote_addr}".into(),
+        ));
+    };
+    debug!("found a session for {remote_asn}@{remote_addr} to send a PDU to");
+    let decoded = BASE64_STANDARD
+        .decode(body.raw_attributes.as_bytes())
+        .unwrap();
+
+    let mut pamap = PaMap::empty();
+    let opa = OwnedPathAttributes::new(PduParseInfo::modern(), decoded);
+    for pa in opa.iter() {
+        if let Ok(pa) = pa {
+            pamap.set_from_enum(pa.to_owned().unwrap());
+        } else {
+            warn!("can't interpret path attribute: {pa:#?}")
+        }
+    }
+
+    if body.prefix.first().is_some_and(|p| p.is_v4()) {
+        let mut builder =
+            UpdateBuilder::<Vec<u8>, Ipv4UnicastNlri>::from_attributes_builder(pamap);
+        if let Err(e) = builder.announcements_from_iter(
+            body.prefix
+                .into_iter()
+                .map(|p| Ipv4UnicastNlri::try_from(p).unwrap()),
+        )
+        //.add_announcement(Ipv4UnicastNlri::try_from(body.prefix).unwrap())
+        {
+            return Err(ApiError::InternalServerError(e.to_string()));
+        };
+        let _ = builder.set_nexthop(routecore::bgp::types::NextHop::Unicast(
+            "10.1.0.254".parse().unwrap(),
+        ));
+        let pdu = match builder.into_message(&SessionConfig::modern()) {
+            Ok(pdu) => pdu,
+            Err(e) => {
+                return Err(ApiError::InternalServerError(e.to_string()));
+            }
+        };
+
+        let bytes = Bytes::from(pdu.as_ref().to_vec());
+        match tx_pdus.try_send(routecore::bgp::message::Message::Update(
+            UpdateMessage::from_octets(bytes, &SessionConfig::modern())
+                .unwrap(),
+        )) {
+            Ok(_) => Ok("ok"),
+            Err(e) => Err(ApiError::InternalServerError(e.to_string())),
+        }
+    } else if body.prefix.first().is_some_and(|p| p.is_v6()) {
+        todo!()
+        //if let Err(e) = builder
+        //    .add_announcement(Ipv6UnicastNlri::try_from(body.prefix).unwrap())
+        //{
+        //    return Err(ApiError::InternalServerError(e.to_string()));
+        //};
+    } else {
+        Err(ApiError::InternalServerError(
+            "no prefixes or unexpected prefix type".into(),
         ))
     }
+
+    //Ok(format!(
+    //    "found a session, TODO send announcement for {} containing {:?}",
+    //    body.prefix, decoded
+    //))
+    //
 }
